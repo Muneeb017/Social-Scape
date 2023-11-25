a7b41760e4698797ea3c206ac96076a7449f6642
Muneeb017 <muneebkhalid0017@gmail.com>
2023-11-26 01:05:18 +0500

    add the FriendsFragment & design, add the LikesFragment & design, home fragment design, message fragment design, search fragment design, add the icons & images & files

---
+++ app/src/main/java/com/muneeb/socialscape/ui/fragments/likes_friend/FriendsFragment.kt
@@ -0,0 +1,22 @@
+package com.muneeb.socialscape.ui.fragments.likes_friend
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.flashbid.luv.extensions.viewBinding
+import com.muneeb.socialscape.R
+import com.muneeb.socialscape.databinding.FragmentFriendsBinding
+import com.muneeb.socialscape.databinding.FragmentLikesBinding
+
+class FriendsFragment : Fragment(R.layout.fragment_friends) {
+
+    private val binding by viewBinding(FragmentFriendsBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/muneeb/socialscape/ui/fragments/likes_friend/LikesFragment.kt
@@ -0,0 +1,19 @@
+package com.muneeb.socialscape.ui.fragments.likes_friend
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.flashbid.luv.extensions.viewBinding
+import com.muneeb.socialscape.R
+import com.muneeb.socialscape.databinding.FragmentLikesBinding
+
+class LikesFragment : Fragment(R.layout.fragment_likes) {
+
+    private val binding by viewBinding(FragmentLikesBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
